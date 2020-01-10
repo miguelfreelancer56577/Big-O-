26e4e992f07704d97f7c5383cf5e26f9b23a5f61
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2020-01-10 13:13:15 -0600

    Finished solution for (Solve Me First) problem

---
+++ src/github/com/miguelfreelancer56577/hackerrank/SolveMeFirst.java
@@ -0,0 +1,27 @@
+package github.com.miguelfreelancer56577.hackerrank;
+/**
+ * This class contains the solution for the problem
+ * @see <a href="https://www.hackerrank.com/challenges/solve-me-first/problem">Solve Me First</a>
+ * 
+ * @author mangelt
+ *
+ */
+public class SolveMeFirst {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		System.out.println(solution(a, b));
+	}
+	
+	/**
+	 * It computes the sum of two integers
+	 * @param a {@literal Integer} with the first number
+	 * @param b {@literal Integer} with the second number
+	 * 
+	 */
+	public static int solution(int a, int b){
+		return a+b;
+	}
+
+}
